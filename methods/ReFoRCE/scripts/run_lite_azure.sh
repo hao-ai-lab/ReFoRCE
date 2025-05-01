2e54c1f3f58c899427d130651f4ed91d47d7cff3
Bruce <m2deng@ucsd.edu>
2025-05-01 22:59:40 +0000

    Update scripts

--- methods/ReFoRCE/scripts/run_lite_azure.sh
+++ methods/ReFoRCE/scripts/run_lite_azure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+API=$1
 # Set up
 gdown 'https://drive.google.com/uc?id=1coEVsCZq-Xvj9p2TnhBFoFTsY-UoYGmG' -O ../../spider2-lite/resource/
 rm -rf ../../spider2-lite/resource/databases/spider2-localdb
@@ -13,16 +13,16 @@ python reconstruct_data.py --example_folder examples_lite --add_description --ma
 python run.py \
     --task lite \
     --db_path examples_lite \
-    --output_path output/o1-preview-lite-log \
-    --model o1-preview \
-    --pre_model o1-preview \
+    --output_path output/${API}-lite-log \
+    --model ${API} \
+    --pre_model ${API} \
     --azure \
-    --schema_linking_model o1-preview \
+    --schema_linking_model ${API} \
     --max_iter 5 \
     --model_vote \
     --num_votes 3 \
     --num_workers 16
 # Evaluation
-python get_metadata.py --result_path output/o1-preview-lite-log --output_path output/o1-preview-lite
+python get_metadata.py --result_path output/${API}-lite-log --output_path output/${API}-lite
 cd ../../spider2-lite/evaluation_suite
-python evaluate.py --mode exec_result --result_dir ../../methods/ReFoRCE/output/o1-preview-lite
+python evaluate.py --mode exec_result --result_dir ../../methods/ReFoRCE/output/${API}-lite
--- methods/ReFoRCE/scripts/run_snow.sh
+++ methods/ReFoRCE/scripts/run_snow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+API=$1
 # Set up
 python spider_agent_setup_snow.py
 # Reconstruct data
@@ -9,15 +9,15 @@ python reconstruct_data.py --example_folder examples_snow --add_description --ma
 python run.py \
     --task snow \
     --db_path examples_snow \
-    --output_path output/o1-preview-snow-log \
-    --model o1-preview \
-    --pre_model o1-preview \
-    --schema_linking_model o1-preview \
+    --output_path output/${API}-snow-log \
+    --model ${API} \
+    --pre_model ${API} \
+    --schema_linking_model ${API} \
     --max_iter 5 \
     --model_vote \
     --num_votes 3 \
     --num_workers 16
 # Evaluation
-python get_metadata.py --result_path output/o1-preview-snow-log --output_path output/o1-preview-snow
+python get_metadata.py --result_path output/${API}-snow-log --output_path output/${API}-snow
 cd ../../spider2-snow/evaluation_suite
-python evaluate.py --mode exec_result --result_dir ../../methods/ReFoRCE/output/o1-preview-snow
+python evaluate.py --mode exec_result --result_dir ../../methods/ReFoRCE/output/${API}-snow
